5f607b15fd0830f1f86cce7e7e0220d78769cf4f
Fahmi Zulkarnain <73563319+freyrtx@users.noreply.github.com>
2021-05-02 13:28:07 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Bunga nasabah = new Bunga();
+        System.out.println("\n===Input Data===");
         nasabah.input();
         nasabah.add();
+        System.out.println("\n===Pilih Jenis Bunga===");
+        nasabah.pilihKredit();
+        System.out.println("\n===Pilih Jenis Bunga===");
+        nasabah.pilihBunga();
         System.out.println("\n=====Data Nasabah=====");
         nasabah.info();
+        System.out.println("\n=====Informasi Bunga=====");
+        nasabah.total();
     }
 }
